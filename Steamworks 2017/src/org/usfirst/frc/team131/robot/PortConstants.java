11f0bbc914e81ab3651c1e7fb89d804229ef552d
EamonSpeerce <easonsu1129@gmail.com>
2017-01-21 13:04:00 -0500

    Created gear flopper class and function to manipulate flopper

--- Steamworks 2017/src/org/usfirst/frc/team131/robot/PortConstants.java
+++ Steamworks 2017/src/org/usfirst/frc/team131/robot/PortConstants.java
@@ -3,4 +3,8 @@ package org.usfirst.frc.team131.robot;
 public class PortConstants {
 	static final int LEFT_SPEED_CONTROLLER_PORT = 0;
 	static final int RIGHT_SPEED_CONTROLLER_PORT = 1;
+	static final int GEAR_FLOPPER_CONTROLLER_PORT = 2;
+	static final int UPPER_GEAR_FLOPPER_LIMIT_SWITCH = 3;
+	static final int LOWER_GEAR_FLOPPER_LIMIT_SWITCH = 4;
+
 }
